ad839b27b60b1da8679707d4cae5a4c804e10283
Franklin <franklin.rosembluth@ucb.edu.bo>
2021-09-08 18:33:01 -0400

    Calcular y mostrar el precio total

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ const estado = document.querySelector("#estado-input");
 const a = document.querySelector("#cantidad");
 const b = document.querySelector("#precio");
 const c = document.querySelector("#estado");
+const d = document.querySelector("#precio_total");
 
 form.addEventListener("submit", (event) => {
 
+    // Variables
+    var precio_total;
+    
     // Denegar la actualizacion de la pagina
     event.preventDefault();
 
-    // Mensaje de alerta
+    // Calcular precio total
+    precio_total = calcular_precio_total(cantidad.value, precio.value);
+
+    // Mensajes
     a.innerHTML = mostrar_cantidad(cantidad.value);
     b.innerHTML = mostrar_precio(precio.value);
     c.innerHTML = mostrar_estado(estado.value);
+    d.innerHTML = mostrar_precio_total(precio_total);
 });
 
 function mostrar_cantidad(cantidad){
@@ -26,5 +34,13 @@ function mostrar_precio(precio){
 }
 
 function mostrar_estado(estado){
-    return "El codigo de estago es : " + estado;
+    return "El codigo de estado es : " + estado;
+}
+
+function mostrar_precio_total(precio_total){
+    return "El precio total es de : " + precio_total + " $";
+}
+
+function calcular_precio_total(cantidad, precio){
+    return cantidad * precio;
 }
